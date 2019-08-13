6933d6bb38e5a39d2e6241dca9cf93efad5ed538
AlekseyM73 <pro.invader73@gmail.com>
2019-08-13 15:30:59 +0400

    add multiselect

--- app/src/main/java/com/example/securerepo/view/NoteListAdapter.java
+++ app/src/main/java/com/example/securerepo/view/NoteListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.securerepo.view;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +10,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.securerepo.R;
 import com.example.securerepo.model.Note;
 import com.example.securerepo.utils.BytesConverter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,29 +25,34 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
 
     private final LayoutInflater layoutInflater;
     private List<Note> notes = Collections.emptyList();
-    private final OnItemClickListener onItemClickListener;
+  // private final OnItemClickListener onItemClickListener;
+    private Context context;
+    private List<Integer> selectedID = new ArrayList<>();
 
     class NoteListHolder extends RecyclerView.ViewHolder {
 
         private CardView cardView;
         private final TextView recyclerviewItemTitle;
+        private final View recyclerItem;
 
         public NoteListHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.card_view);
             recyclerviewItemTitle = itemView.findViewById(R.id.recyclerviewItemTitle);
+            recyclerItem = itemView.findViewById(R.id.recyclerviewItem);
         }
 
-        public void onBind (int noteId, NoteListAdapter.OnItemClickListener onItemClickListener){
+       /* public void onBind (int noteId, NoteListAdapter.OnItemClickListener onItemClickListener){
             if (onItemClickListener != null ){
                 itemView.setOnClickListener(v -> onItemClickListener.onItemClick(noteId));
             }
-        }
+        }*/
     }
 
-    public NoteListAdapter(Context context, OnItemClickListener onItemClickListener) {
+    public NoteListAdapter(Context context) {
         layoutInflater = LayoutInflater.from(context);
-       this.onItemClickListener = onItemClickListener;
+        this.context = context;
+      // this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
@@ -58,11 +67,33 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
         Note current = notes.get(position);
         char[] text = BytesConverter.bytesToChar(current.getTitle());
         holder.recyclerviewItemTitle.setText(text,0,text.length);
-        holder.onBind(current.getId(), onItemClickListener);
+       // holder.onBind(current.getId(), onItemClickListener);
+        int id = current.getId();
+        int color = holder.cardView.getCardBackgroundColor().getDefaultColor();
+        Log.d("COLOR",color+"");
+        if (selectedID.contains(id)){
+            holder.cardView.setCardBackgroundColor(context.getResources()
+                    .getColor(R.color.colorAccent));
+        } else {
+            holder.cardView.setCardBackgroundColor(context
+                    .getResources().getColor(android.R.color.background_light));
+        }
+    }
+    public List<Note> getNotesfromAdapter(){
+        return notes;
+    }
+
+    public Note getNotefromAdapter (int position){
+        return notes.get(position);
     }
 
     public void setNotes (List<Note> notes){
         this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public void setSelectedID (List<Integer> id){
+        this.selectedID = id;
         notifyDataSetChanged();
     }
 
@@ -71,9 +102,9 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
         return notes.size();
     }
 
-    public interface OnItemClickListener {
+    /*public interface OnItemClickListener {
         void onItemClick (int nodeId);
-    }
+    }*/
 }
 
 
--- app/src/main/java/com/example/securerepo/view/RecyclerViewNoteListActivity.java
+++ app/src/main/java/com/example/securerepo/view/RecyclerViewNoteListActivity.java
@@ -2,6 +2,7 @@ package com.example.securerepo.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,12 +16,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.securerepo.R;
+import com.example.securerepo.model.Note;
 import com.example.securerepo.viewmodel.RecyclerViewModel;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class RecyclerViewNoteListActivity extends AppCompatActivity {
+
+public class RecyclerViewNoteListActivity extends AppCompatActivity
+        implements ActionMode.Callback {
 
     private final String NOTE_ID = "Id";
     private NoteListAdapter adapter;
@@ -28,6 +34,10 @@ public class RecyclerViewNoteListActivity extends AppCompatActivity {
     private RecyclerViewModel recyclerViewModel;
     private char [] password;
     private BottomNavigationDrawerFragment bottomNavigationDrawerFragment;
+    private RecyclerView recyclerView;
+    private ActionMode actionMode;
+    private boolean isMultiSelect;
+    private List<Integer> selectedID = new ArrayList<>();
 
 
     @Override
@@ -37,17 +47,19 @@ public class RecyclerViewNoteListActivity extends AppCompatActivity {
 
         password = getIntent().getCharArrayExtra(PASSWORD);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
         BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(fabListener);
         setSupportActionBar(bottomAppBar);
-        adapter = new NoteListAdapter(this, noteId -> {
+       /* adapter = new NoteListAdapter(this, noteId -> {
             Intent intent = new Intent(RecyclerViewNoteListActivity.this, DetailNoteActivity.class);
             intent.putExtra(NOTE_ID, noteId);
             intent.putExtra(PASSWORD, password);
             startActivity(intent);
-        });
+        });*/
+
+        adapter = new NoteListAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewModel = ViewModelProviders.of(this).get(RecyclerViewModel.class);
@@ -66,6 +78,60 @@ public class RecyclerViewNoteListActivity extends AppCompatActivity {
        recyclerViewModel.getNotes(password).observe(this, notes -> {
             adapter.setNotes(notes);
         });
+        recyclerView.addOnItemTouchListener(new RecyclerClickListener
+                (this, recyclerView,
+                        new RecyclerClickListener.OnItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                if (isMultiSelect){
+                    multiSelect(position);
+                } else {
+                    Intent intent = new Intent(RecyclerViewNoteListActivity.this, DetailNoteActivity.class);
+                    intent.putExtra(NOTE_ID, adapter.getNotefromAdapter(position).getId());
+                    intent.putExtra(PASSWORD, password);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                if (!isMultiSelect){
+                    selectedID = new ArrayList<>();
+                    isMultiSelect = true;
+
+                    if (actionMode == null){
+                        actionMode = startActionMode
+                                (RecyclerViewNoteListActivity.this);
+                    }
+                }
+
+                multiSelect(position);
+            }
+        }));
+
+
+
+    }
+
+    private void multiSelect(int position) {
+        Note note = adapter.getNotefromAdapter(position);
+        if (note != null){
+            if (actionMode != null) {
+                if (selectedID.contains(note.getId()))
+                    selectedID.remove(Integer.valueOf(note.getId()));
+                else
+                    selectedID.add(note.getId());
+
+                if (selectedID.size() > 0)
+                    actionMode.setTitle(String.valueOf(selectedID.size()));
+                else{
+                    actionMode.setTitle("");
+                    actionMode.finish();
+                }
+                adapter.setSelectedID(selectedID);
+
+            }
+        }
     }
 
     @Override
@@ -84,24 +150,39 @@ public class RecyclerViewNoteListActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        MenuInflater inflater = mode.getMenuInflater();
+        inflater.inflate(R.menu.bottom_nav_drawer_menu, menu);
+        return true;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-
+    public boolean onActionItemClicked(ActionMode mode, MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.bottom_app_bar_menu_delete:
+                //just to show selected items.
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Note note : adapter.getNotesfromAdapter()) {
+                    if (selectedID.contains(note.getId()))
+                        stringBuilder.append("\n").append(note.getTitle());
+                }
+                Toast.makeText(this, "Selected items are :"
+                        + stringBuilder.toString(), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onDestroyActionMode(ActionMode mode) {
+        actionMode = null;
+        isMultiSelect = false;
+        selectedID.clear();
+        adapter.setSelectedID(new ArrayList<Integer>());
     }
 }
