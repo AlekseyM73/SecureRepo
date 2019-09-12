3803ab745e8ead03d0ac7378958bedb5077f387c
Aleksey <pro.invader73@gmail.com>
2019-09-12 22:04:23 +0400

    in process

--- app/src/main/java/com/example/securerepo/application/App.java
+++ app/src/main/java/com/example/securerepo/application/App.java
@@ -23,15 +23,10 @@ public class App extends Application {
     public static NotesDatabase notesDatabase;
     public static Cipher cipher;
     public static SecretKeySpec secretKeySpec;
-    public static Lifecycle lifecycle;
-    public static App.AppLifeCycleListener appLifeCycleListener;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        lifecycle = ProcessLifecycleOwner.get().getLifecycle();
-        appLifeCycleListener = new AppLifeCycleListener();
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         boolean isDarkTheme = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_dark_theme", false);
@@ -42,12 +37,6 @@ public class App extends Application {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
 
-        boolean isExitBackground = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_exit_background", false);
-
-        if (isExitBackground){
-            lifecycle.addObserver(appLifeCycleListener);
-        }
-
         notesDatabase = NotesDatabase.getDataBase(this);
         try {
             cipher = Cipher.getInstance("AES");
@@ -56,19 +45,4 @@ public class App extends Application {
         }
     }
 
-    class AppLifeCycleListener implements LifecycleObserver {
-
-        @OnLifecycleEvent(Lifecycle.Event.ON_START)
-        public void onMoveToForeground(){
-
-        }
-        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-        public void onMoveToBackground(){
-            Log.d("DEBUG","background");
-            Intent intent = new Intent(getApplicationContext(), EnterPasswordActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }
-
-    }
 }
